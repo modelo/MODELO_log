0942f5f1f395b723079463baef509f23ebccf165
timqian <timqian92@qq.com>
2016-10-11 14:57:55 +0800

    format code to modelo coding style

--- getIP.js
+++ getIP.js
@@ -2,18 +2,18 @@ var os = require('os');
 var ifaces = os.networkInterfaces();
 
 function getIP() {
-  var ip = '';
+    var ip = '';
 
-  Object.keys(ifaces).forEach(function (ifname) {
-    ifaces[ifname].forEach(function (iface) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      if (iface.family === 'IPv4' && iface.internal === false) {
-        ip = iface.address;
-      }
+    Object.keys(ifaces).forEach(function(ifname) {
+        ifaces[ifname].forEach(function(iface) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            if (iface.family === 'IPv4' && iface.internal === false) {
+                ip = iface.address;
+            }
+        });
     });
-  });
 
-  return ip;
+    return ip;
 }
 
 module.exports = getIP;
--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 var winston = require('winston');
-require('winston-daily-rotate-file'); 
+require('winston-daily-rotate-file');
 var getIP = require('./getIP');
 var path = require('path');
 var fs = require('fs');
 
 var logPath = path.join(process.cwd(), '/logs')
 if (!fs.existsSync(logPath)) {
-  fs.mkdirSync(logPath);
+    fs.mkdirSync(logPath);
 }
 
 var ip = getIP();
 
-var logger = new (winston.Logger)({  
-  transports: [
-    new (winston.transports.Console)({
-      timestamp: timestamp
-    }),
-    new (winston.transports.DailyRotateFile)({
-      timestamp: timestamp,
-      filename: path.join(logPath, ip),  
-      datePattern: '-yyyy-MM-dd.txt',  // switch file daily
-      maxsize: 1024 * 1024 * 50, // 50MB
-      json: false
-    })
-  ]
-});
-
-function timestamp () {
-  var now = new Date()
-  return now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+var timstamp = function() {
+    var now = new Date()
+    return now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
 }
+
+var logger = new(winston.Logger)({
+    transports: [
+        new(winston.transports.Console)({
+            timestamp: timestamp
+        }),
+        new(winston.transports.DailyRotateFile)({
+            timestamp: timestamp,
+            filename: path.join(logPath, ip),
+            datePattern: '-yyyy-MM-dd.txt', // switch file daily
+            maxsize: 1024 * 1024 * 50, // 50MB
+            json: false
+        })
+    ]
+});
 
 module.exports = logger;
